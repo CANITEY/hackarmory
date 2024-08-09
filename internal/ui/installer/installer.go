073420f3648a67b3633bb49d3d2d7932206057fb
canitey <mohamedmed01345@gmail.com>
2024-08-10 00:47:41 +0300

    integrated helpers and used the tools from the initializer function

--- internal/ui/installer/installer.go
+++ internal/ui/installer/installer.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/CANITEY/hackarmory/internal/helpers"
 	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -18,8 +19,9 @@ const (
 
 var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
 
+// tools status 0 not installed, 1 installing, 2 installed
 type Model struct {
-	tools map[string]bool
+	tools    map[string]int
 	progress progress.Model
 }
 
@@ -33,9 +35,9 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, tea.Quit
 
 	case tea.WindowSizeMsg:
-		m.progress.Width = msg.Width - padding*2 - 4
+		m.progress.Width = msg.Width - padding * 2
 		if m.progress.Width > maxWidth {
-			m.progress.Width = maxWidth
+			m.progress.Width = msg.Width - 2 * padding
 		}
 		return m, nil
 
@@ -67,6 +69,7 @@ func (m Model) View() string {
 		pad + helpStyle("Press any key to quit")
 }
 
+// user defined functions
 func tickCmd() tea.Cmd {
 	return tea.Tick(time.Second*1, func(t time.Time) tea.Msg {
 		return tickMsg(t)
@@ -74,7 +77,9 @@ func tickCmd() tea.Cmd {
 }
 
 func NewModel(tools []string) *Model {
-	return &Model {
+	toolsMap := helpers.StoMInt(tools)
+	return &Model{
 		progress: progress.New(progress.WithDefaultGradient()),
+		tools:    toolsMap,
 	}
 }
