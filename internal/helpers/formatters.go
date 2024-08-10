c003535872170b74ca930a5e2006ef0f10b2d71a
canitey <mohamedmed01345@gmail.com>
2024-08-10 16:18:13 +0300

    added styling to formatter

---
+++ internal/helpers/formatters.go
@@ -0,0 +1,61 @@
+package helpers
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+func NewToolLogger(tools map[string]int) *ToolLogger {
+	waiting := lipgloss.NewStyle().Foreground(lipgloss.Color("1")).Render
+	installing := lipgloss.NewStyle().Background(lipgloss.Color("1")).Render
+	done := lipgloss.NewStyle().Background(lipgloss.Color("1")).Render
+	error := lipgloss.NewStyle().Background(lipgloss.Color("1")).Render
+	style := style{
+		waiting:    waiting,
+		done:       done,
+		installing: installing,
+		err:        error,
+	}
+	return &ToolLogger{
+		Tools: tools,
+		style: style,
+	}
+}
+
+type style struct {
+	waiting, installing, done, err func(...string) string
+}
+
+type ToolLogger struct {
+	Tools map[string]int
+	style style
+}
+
+func (t *ToolLogger) Log() string {
+	buf := strings.Builder{}
+
+	tools := make([]string, 0)
+
+	for tool, _ := range t.Tools {
+		tools = append(tools, tool)
+	}
+
+	slices.Sort(tools)
+
+	for _, tool := range tools {
+		switch t.Tools[tool] {
+		case -1:
+			buf.WriteString(t.style.waiting(fmt.Sprintf("[W] %v\n", tool)))
+		case 0:
+			buf.WriteString(t.style.installing(fmt.Sprintf("[I] %v\n", tool)))
+		case 1:
+			buf.WriteString(t.style.done(fmt.Sprintf("[D] %v\n", tool)))
+		case 2:
+			buf.WriteString(t.style.err(fmt.Sprintf("[E] %v\n", tool)))
+		}
+	}
+	return buf.String()
+}
