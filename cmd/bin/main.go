a9a134be31c65dff091958ccc3d97bcb559d37d3
canitey <mohamedmed01345@gmail.com>
2024-08-16 15:37:59 +0300

    configured main to run the installer api

--- cmd/bin/main.go
+++ cmd/bin/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"github.com/CANITEY/hackarmory/internal/ui/index"
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/CANITEY/hackarmory/internal/installs"
+	// "github.com/CANITEY/hackarmory/internal/ui/index"
+	// tea "github.com/charmbracelet/bubbletea"
 )
-
 
 // link all UIs together
 func main() {
-	p := tea.NewProgram(index.NewModel("1.0.0"), tea.WithAltScreen())
-	_, err := p.Run()
-	if err != nil {
-		panic(err)
-	}
+	// p := tea.NewProgram(index.NewModel("1.0.0"), tea.WithAltScreen())
+	// _, err := p.Run()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	installs.CreateToolsDir()
 }
