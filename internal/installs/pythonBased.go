55a57051c8a9fa0bc90b73b8daaf95c73b6d2ac5
canitey <mohamedmed01345@gmail.com>
2024-08-17 15:44:03 +0300

    changed arjun to use helper.executer

--- internal/installs/pythonBased.go
+++ internal/installs/pythonBased.go
@@ -1,20 +1,26 @@
 package installs
 
 import (
-	"os/exec"
-	"path"
-	"strings"
+	"github.com/CANITEY/hackarmory/internal/helpers"
 )
 
-func arjun() (error) {
-	cmd := exec.Cmd{}
+func arjun() error {
 	toolsDir, err := GetToolsDir()
 	if err != nil {
 		return err
 	}
-	cmd.Dir  = toolsDir
-	cmd.Args = strings.Split("git clone https://github.com/s0md3v/Arjun", " ")
-	if err := cmd.Run(); err != nil {
+	exec := helpers.NewExecuter(toolsDir)
+	err = exec.Execute("git clone https://github.com/s0md3v/Arjun", "")
+	if err != nil {
+		return err
+	}
+
+	err = exec.Execute("python3 setup.py install", "Arjun/")
+	if err != nil {
+		return err
+	}
+	return nil
+}
 		return err
 	}
 
