f94b0ce1e12735772eb59a7eb5dd9636718ca35c
canitey <mohamedmed01345@gmail.com>
2024-08-22 00:21:55 +0300

    the journey of trying the AddPath helper

--- cmd/bin/test.go
+++ cmd/bin/test.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/CANITEY/hackarmory/internal/helpers"
 )
 
 func main() {
+	fmt.Println(helpers.AddPath("hahahah"))
 	fmt.Println("TEST MAIN FILE")
 }
