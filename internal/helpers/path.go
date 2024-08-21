35263d75dfe688d82d19450c71c85d250bc5c77e
canitey <mohamedmed01345@gmail.com>
2024-08-22 00:04:42 +0300

    added the write path into the add path function

--- internal/helpers/path.go
+++ internal/helpers/path.go
@@ -6,6 +6,7 @@ import (
 	"path"
 	"slices"
 	"strings"
+
 )
 
 func CheckPath(p string) bool {
@@ -15,7 +16,6 @@ func CheckPath(p string) bool {
 }
 
 func AddPath(p string) (bool, error) {
-	// TODO: add the function that adds path to path variable
 	if ok := CheckPath(p); !ok {
 		return true, nil
 	}
@@ -30,24 +30,13 @@ func AddPath(p string) (bool, error) {
 
 		return true, nil
 	case strings.Contains(shellName, "zsh"):
-		// TODO: Make the writer write between the # START and # END
-		home, _ := os.UserHomeDir()
-		zshrc := path.Join(home, ".zshrc")
-		file, err := os.OpenFile(zshrc, os.O_CREATE | os.O_RDWR, 0644)
-		if err != nil {
+		if err := writePath(".zshrc", p); err != nil {
 			return false, err
 		}
-		data := fmt.Sprintf("\n\n# START of hackarmory modification \nexport PATH=$PATH:%v\n\n# END of hackarmory\n", p)
-		file.WriteString(data)
 	case strings.Contains(shellName, "bash"):
-		home, _ := os.UserHomeDir()
-		zshrc := path.Join(home, ".bashrc")
-		file, err := os.OpenFile(zshrc, os.O_CREATE | os.O_RDWR, 0644)
-		if err != nil {
+		if err := writePath(".bashrc", p); err != nil {
 			return false, err
 		}
-		data := fmt.Sprintf("\n\n# START of hackarmory addition \nexport PATH=$PATH:%v\n\n# END of hackarmory\n", p)
-		file.WriteString(data)
 	}
 
 	return true, nil
